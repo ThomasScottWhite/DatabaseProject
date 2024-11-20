7ee3daccebed9f5881c5cc850b2bdd0f7558df2f
Connor Warren <paradoxoddity04@gmail.com>
2024-11-20 00:47:22 -0600

    Add a lot more test data

--- api/db/sql/test_data.sql
+++ api/db/sql/test_data.sql
@@ -40,4 +40,25 @@ INSERT INTO Member (Chapter_ID, Email, Fname, Lname, DOB, Is_Chapter_Admin, Memb
     (3, 'badhabits@hotmail.com', 'bad', 'babits', '2000-01-01', TRUE, 'FIXME', '888-888-8888');
 
 
-    
+INSERT INTO Bill (Chapter_ID, Bill_ID, Amount, Amount_Paid, 'desc', Due_Date, Issue_Date, Is_External)
+    VALUES (1, 1234, 10, 0, 'No description', '2024-01-02 00:00:00', '2024-01-01', FALSE),
+    (1, 2345, 10, 0, 'No description', '2024-01-02 00:00:00', '2024-01-01', TRUE);
+
+--No member paying?
+INSERT INTO External_Bill (Bill_ID, Chapter_Contact, Payor_Name, P_Billing_Address, P_Email, P_Phone_Num)
+    VALUES (2345, 'FIXME', 'FIXME', 'FIXME', 'fix@me.com', 'FIX-ME1-2345');
+
+INSERT INTO Internal_Bill (Bill_ID, Member_Email)
+    VALUES (1234, 'dog@dogmail.com');
+
+--Make payment ID serial?
+INSERT INTO Payment_Info (Member_Email, Payment_ID, Nickname)
+    VALUES ('dog@dogmail.com', 1, 'Dogpal'),
+    ('cat@catmail.com', 2, 'catpal');
+
+INSERT INTO Bank_Account (Payment_ID, Account_Num, Routing_Num)
+    VALUES (1, 123456789, 987654321);    
+
+--Exp_Date should have constraint maybe?
+INSERT INTO Card (Payment_ID, Card_Num, Security_Code, Exp_Date, Name)
+    VALUES (2, 13579, 024, 07-27, 'mastercat');
