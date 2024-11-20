0d896bc8099cb2c1bc277ee44c95ed1328d15009
Connor Warren <paradoxoddity04@gmail.com>
2024-11-19 19:59:40 -0600

    Fixing Port Consistency

--- greek-management-studio-frontend/vite.config.js
+++ greek-management-studio-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Proxy all requests starting with "/api" to the backend
       '/api': {
-        target: 'http://localhost:8080', // Backend server URL
+        target: 'http://localhost:6969', // Backend server URL
         changeOrigin: true,             // Changes the origin of the request to the target
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: remove "/api" prefix when forwarding
       },
