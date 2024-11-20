888890996bb86eb28026bc3b5d3bf962a6d6e6f1
Harry Heitmeier <harry.heitmeier@gmail.com>
2024-11-19 22:35:31 -0600

    update table manager to use sqlalchemy's built-in 'TableManager' equivalent

--- api/db/tables.py
+++ api/db/tables.py
@@ -12,18 +12,15 @@ _TABLES_QUERY = "SELECT table_name FROM information_schema.tables WHERE table_sc
 class _TableManager:
 
     def __init__(self):
-        self.tables: dict[str, Table] = dict()
+        self.meta = MetaData()
         self.load_tables()
 
     def load_tables(self):
-        eng = engine.get_engine()
-        with eng.connect() as conn:
-            for (table_name,) in conn.execute(text(_TABLES_QUERY)).all():
-                self.tables[table_name] = Table(table_name, _META, autoload_with=eng)
+        self.meta.reflect(bind=engine.get_engine())
 
     def __getattr__(self, name) -> Table | Any:
-        if name in self.tables:
-            return self.tables[name]
+        if name in self.meta.tables:
+            return self.meta.tables[name]
         else:
             return super().__getattribute__(name)
 
